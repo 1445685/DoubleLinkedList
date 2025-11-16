919188ea483b379855fe093df27385cf6ea64f61
no_signal_here <1445685@gmail.com>
2025-11-16 20:28:02 -0300

    Refactorizacion de las clases y documentacion

--- src/doubleLinkedList/DoubleLinkedList.java
+++ src/doubleLinkedList/DoubleLinkedList.java
@@ -1,115 +1,323 @@
 package doubleLinkedList;
 
+import java.util.NoSuchElementException;
+
+/**
+ * Class that implements the logic of a simple doubly linked list
+ * @see <a src="https://en.wikipedia.org/wiki/Doubly_linked_list">Wikipedia article on doubly linked lists</a>
+ * 
+ * @author no_$ignal_here
+ * 
+ * @version 0.5
+ * */
+
 public class DoubleLinkedList {
 	private int lenght = 0;
 	private Node firstNode = null;
 	private Node lastNode = null;
-	private Node actualNode = null;
 
 	// ------ constructors
+	/**
+	 * Build a new, completely empty list
+	 * */
 	public DoubleLinkedList() {}
+	/**
+	 * Build a new list with an initial node
+	 * 
+	 * @param firstNode The first node in the list
+	 * */
 	public DoubleLinkedList(Node firstNode) {
 		this.firstNode = firstNode;
 		this.lastNode = firstNode;
 		this.lenght++;
 	}
+	/**
+	 * Build a new list containing a node with the specified value
+	 * 
+	 * @param valueValue contained in the first node
+	 * */
 	public DoubleLinkedList(int value) {
 		this(new Node(value));
 	}
 
 	// ------ methods
-	public void add(Node newNode) {
-		newNode.setNextNode(this.actualNode.getNextNode());
-		this.actualNode.setNextNode(newNode);
-	}
-	public void add(int value) {
-		this.add(new Node(value));
-	}
-	public void add(Node n, int i) throws IndexOutOfBoundsException {
-		if (i > this.getLength()) 
-			throw new IndexOutOfBoundsException("Index greater than the number of items in the list");
+	/**
+	 * This method searches the list for the node passed as an argument
+	 * 
+	 * @param node Node being searched for in the list
+	 * 
+	 * @return Returns the index at which the node appears in the list
+	 *  If the node does not exist in the list, returns -1
+	 *  
+	 * @throws IllegalStateException Throws an exception when trying to access an empty list.
+	 * */
+	private int existsInList(Node node) throws IllegalStateException {
+		if (this.lenght == 0) {throw new IllegalStateException("Empty list");}
 		
-		this.actualNode = this.firstNode;
+		int index = 1;
+		Node aux = this.getFirstNode();
 		
-		for (int index = 1; index <= i; index++) {
-			this.actualNode = this.actualNode.getNextNode();
+		while (index <= this.getLength()) {
+			if(aux.equals(node)) {return index;}
+			aux = aux.getNextNode();
+			
+			index++;
 		}
 		
-		this.add(n);
-	}
-	public void addFirst(Node n) {
-		n.setNextNode(this.firstNode);
-		this.setFirstNode(n);
-	}
-	public void addFirst(int value) {
-		this.addFirst(new Node(value));
-	}	
-	public void addLast(Node n) {
-		n.setPreviousNode(this.getLastNode());
-		this.setLastNode(n);
-	}
-	public void addLast(int value) {
-		this.addLast(new Node(value));
-	}
-
-	public void remove() {
-		Node aux = this.actualNode;
-		this.actualNode = this.actualNode.getNextNode();
-		
-		aux.getNextNode().setPreviousNode(aux.getPreviousNode());
-		aux.getPreviousNode().setNextNode(aux.getNextNode());
-	}
-	public void remove(int i) throws IndexOutOfBoundsException{
-		if (i > this.lenght) 
-			throw new IndexOutOfBoundsException("Index greater than the number of items in the list");
-		
-		this.actualNode = this.firstNode;
-		
-		for(int index = 1; index <= i; index++) {
-			this.actualNode = this.actualNode.getNextNode();
-		}
-		
-		this.remove();
-	}
-	public void romoveFirst() {
-		this.setFirstNode(this.getFirstNode().getNextNode());
-	}
-	public void removeLast() {
-		this.setLastNode(this.getLastNode().getPreviousNode());
+		return -1;
 	}
 	
-	public int search(int i) throws IndexOutOfBoundsException{
-		if (i > this.lenght) 
-			throw new IndexOutOfBoundsException("Index greater than the number of items in the list");
-		
-		this.actualNode = this.firstNode;
-		
-		for(int index = 0; index < i; index++) {
-			this.actualNode = this.actualNode.getNextNode();
+	/**
+	 * Insert a node at the first position in the list and shift the other elements
+	 * 
+	 * @param newNode Node to be inserted
+	 * 
+	 * @return Nothing at the moment
+	 * */
+	public void insertFirstNode(Node newNode) {
+		if (this.lenght == 0) {
+			this.firstNode = newNode;
+			this.lastNode = newNode;
+		} else {
+			newNode.setNextNode(this.getFirstNode());
+			this.getFirstNode().setPreviousNode(newNode);
+			this.firstNode = newNode;
 		}
 		
-		return this.actualNode.getValue();
+		this.lenght++;
 	}
-
-	public void cleanList() {
-		this.firstNode = null;
-		this.lastNode = null;
-		this.actualNode = null;
+	/**
+	 * Insert a node at the end of the list
+	 * 
+	 * @param newNode Node to be inserted
+	 * 
+	 * @return Nothing at the moment
+	 * */
+	public void insertLastNode(Node newNode) {
+		if (this.lenght == 0) {
+			this.firstNode = newNode;
+			this.lastNode = newNode;
+		} else {
+			this.getLastNode().setNextNode(newNode);
+			newNode.setPreviousNode(this.getLastNode());
+			this.lastNode = newNode;
+		}
+		
+		this.lenght++;
+	}
+	/**
+	 * Insert a node before the specified node
+	 * 
+	 * @param node Node used as a reference when inserting a new node
+	 * @param newNode Node to be inserted 
+	 * 
+	 * @return Nothing at the moment
+	 * 
+	 * @throws NoSuchElementException Exception indicating that the node used as a reference does not exist in the list
+	 * */
+	public void insertBeforeNode(Node node, Node newNode) throws NoSuchElementException {
+		int indexNode = this.existsInList(node);
+		if (indexNode == -1) {throw new NoSuchElementException("The node indica does not exist in the list");}
+		if (indexNode == 1) {
+			this.insertFirstNode(newNode);
+		} else {
+			Node previousNode = node.getPreviousNode();
+			
+			newNode.setNextNode(node);
+			newNode.setPreviousNode(previousNode);
+			
+			previousNode.setNextNode(newNode);
+			node.setPreviousNode(newNode);
+			
+			this.lenght++;
+		}
+	}
+	/**
+	 * Insert a node after the specified node
+	 * 
+	 * @param node Node used as a reference when inserting a new node
+	 * @param newNode Node to be inserted 
+	 * 
+	 * @return Nothing at the moment
+	 * 
+	 * @throws NoSuchElementException Exception indicating that the node used as a reference does not exist in the list
+	 * */
+	public void insertAfterNode(Node node, Node newNode) throws NoSuchElementException {
+		int indexNode = this.existsInList(node);
+		if (indexNode == -1) {throw new NoSuchElementException("The node indica does not exist in the list");}
+		if (indexNode == this.getLength()) {
+			this.insertLastNode(newNode);
+		} else {
+			Node nextNode = node.getNextNode();
+			
+			newNode.setNextNode(nextNode);
+			newNode.setPreviousNode(node);
+			
+			nextNode.setPreviousNode(newNode);
+			node.setNextNode(newNode);
+			this.lenght++;
+		}
+	}
+	
+	/**
+	 * Remove the first node and shift the list
+	 * 
+	 * @return Nothing at the moment
+	 * 
+	 * @throws IllegalStateException Throws an exception when trying to access an empty list.
+	 * */
+	public void removeFirstNode() throws IllegalStateException {
+		if (this.lenght == 0) {throw new IllegalStateException("Empty list");}
+		
+		if (this.getLength() == 1) {
+			this.firstNode = null;
+			this.lastNode = null;
+		} else {
+			this.firstNode = this.getFirstNode().getNextNode();
+			this.getFirstNode().setPreviousNode(null);
+		}
+		
+		this.lenght--;
+	}
+	/**
+	 * Remove the last node and shift the list
+	 * 
+	 * @return Nothing at the moment
+	 * 
+	 * @throws IllegalStateException Throws an exception when trying to access an empty list.
+	 * */
+	public void removeLastNode() throws IllegalStateException {
+		if (this.lenght == 0) {throw new IllegalStateException("Empty list");}
+		
+		if (this.lenght == 1) {
+			this.firstNode = null;
+			this.lastNode = null;
+		} else {
+			this.lastNode = this.getLastNode().getPreviousNode();
+			this.getLastNode().setNextNode(null);
+		}
+		
+		this.lenght--;
+	}
+	/**
+	 * Remove the specified node from the list
+	 * 
+	 * @param node Node to be removed from the list
+	 * 
+	 * @return Nothing at the moment
+	 * 
+	 * @throws NoSuchElementException Exception indicating that the node to be deleted does not exist in the list
+	 * @throws IndexOutOfBoundsException If the first node is passed as a reference, an exception will be raised because there is no node preceding it.
+	 * */
+	public void removeBeforeNode(Node node) throws NoSuchElementException, IndexOutOfBoundsException {
+		int indexNode = this.existsInList(node);
+		if (indexNode == -1) {throw new NoSuchElementException("The node indica does not exist in the list.");}
+		if (indexNode == 1) {throw new IndexOutOfBoundsException("This attempts to delete the node before the first one, which does not exist.");}
+		if (indexNode == 2) {
+			this.removeFirstNode();
+		} else {
+			Node previousNode = node.getPreviousNode().getPreviousNode();
+			
+			node.setPreviousNode(previousNode);
+			previousNode.setNextNode(node);
+			
+			this.lenght--;
+		}
+	}
+	/**
+	 * Remove the specified node from the list
+	 * 
+	 * @param node Node to be removed from the list
+	 * 
+	 * @return Nothing at the moment
+	 * 
+	 * @throws NoSuchElementException Exception indicating that the node to be deleted does not exist in the list
+	 * @throws IndexOutOfBoundsException If the last node is passed as a reference, an exception is raised because an attempt is made to delete a node that does not exist
+	 * */
+	public void removeAfterNode(Node node) throws NoSuchElementException, IndexOutOfBoundsException {
+		int indexNode = this.existsInList(node);
+		if (indexNode == -1) {throw new NoSuchElementException("The node indica does not exist in the list.");}
+		if (indexNode == this.lenght) {throw new IndexOutOfBoundsException("This attempts to delete the node following the last one, which does not exist");}
+		if (indexNode == this.getLength()-1) {
+			this.removeLastNode();
+		} else {
+			Node nextNode = node.getNextNode().getNextNode();
+			
+			node.setNextNode(nextNode);
+			nextNode.setPreviousNode(node);
+			
+			this.lenght--;
+		}
 	}
 	
 	// ------ gets and sets
+	/**
+	 * Returns the size of the list
+	 * 
+	 * @return size of list
+	 * */
 	public int getLength() {return this.lenght;} 
 	
+	/**
+	 * Returns the first node in the list
+	 * 
+	 * @return Reference to the first node of the list
+	 * */
 	public Node getFirstNode() {return this.firstNode;}
-	public void setFirstNode(Node newFirstNode) {this.firstNode = newFirstNode;}
+	/**
+	 * Modify the first node in the list
+	 * 
+	 * @param newFirstNode The node that will replace the first node in the list
+	 * */
+	public void setFirstNode(Node newFirstNode) {
+		// Reference to the second node in the list or to null
+		Node nextNodeOfFirstNode = this.getFirstNode().getNextNode();
+		
+		//  Modify the previous and next node references if there is a second node in the list.
+		//  If there is no second node in the list, then the reference to lastNode must also be
+		// modified because there is only one node in the list.
+		if (nextNodeOfFirstNode != null) {
+			nextNodeOfFirstNode.setPreviousNode(newFirstNode);
+			newFirstNode.setNextNode(nextNodeOfFirstNode);
+		}
+		
+		this.firstNode = newFirstNode;
+	}
 	
+	/**
+	 * Returns the last node in the list
+	 * 
+	 * @return Reference to the last node of the list
+	 * */
 	public Node getLastNode() {return this.lastNode;}
-	public void setLastNode(Node newLastNode) {this.lastNode = newLastNode;}
-	
-	public Node getActualNode() {return this.actualNode;}
-	public void setActualNode(Node newActualNode) {this.actualNode = newActualNode;}
+	/**
+	 * Modify the last node in the list
+	 * 
+	 * @param newLastNode The node that will replace the last node in the list
+	 * */
+	public void setLastNode(Node newLastNode) {
+		// Reference to the penultimate node in the list
+		Node previousNodeOfLastNode = this.getLastNode().getPreviousNode();
+		
+		//  If there is a penultimate node, then the previous and next node references are modified. 
+		//  If there is no penultimate node, then the list has only one node and the firstNode 
+		// reference must also be modified.
+		if (previousNodeOfLastNode != null) {
+			newLastNode.setPreviousNode(previousNodeOfLastNode);
+			previousNodeOfLastNode.setNextNode(newLastNode);
+		} else {
+			this.firstNode = newLastNode;
+		}
+		
+		this.lastNode = newLastNode;
+	}
 	
 	// ------ toString and equals
+	/**
+	 * Displays useful information from the list: firstNode, lastNode, and length.
+	 * 
+	 * @return String with the information from the list
+	 * */
 	public String toString() {
 		return "[length: " + this.lenght +
 				"\n" + "\tfirst node: " + this.firstNode +
@@ -117,23 +325,31 @@ public class DoubleLinkedList {
 				"]\n";
 	}
 	
+	/**
+	 * Check if two lists are equal by iterating and comparing each node
+	 * 
+	 * @param otherObject The other object with which you want to compare the list
+	 * 
+	 * @return Returns a boolean indicating whether the objects are the same (true) or different (false).
+	 * */
 	public boolean equals(Object otherObject) {
 		if (this == otherObject) {return true;}
 		if (otherObject == null) {return false;}
 		if (!(otherObject instanceof DoubleLinkedList other)) {return false;}
 		if (this.lenght != other.getLength()) {return false;}
 		
-		boolean result = false;
 		int index = 0;
 		
-		this.actualNode = this.firstNode;
-		Node otherActualNode = other.getActualNode();
+		Node auxNode = this.firstNode;
+		Node otherActualNode = other.getFirstNode();
 		
-		while (index < this.lenght && !result) {
-			if (this.actualNode.getValue() != otherActualNode.getValue()) {return false;}
+		while (index < this.lenght) {
+			if (!auxNode.equals(otherActualNode)) {return false;}
 			
-			this.actualNode = this.actualNode.getNextNode();
+			auxNode = auxNode.getNextNode();
 			otherActualNode = otherActualNode.getNextNode();
+			
+			index++;
 		}
 		return true;
 	}
--- src/doubleLinkedList/Node.java
+++ src/doubleLinkedList/Node.java
@@ -1,35 +1,103 @@
 package doubleLinkedList;
 
+/**
+ * Class representing a node in a list
+ * 
+ * @author no_$ignal_here
+ * 
+ * @version 0.5
+ * */
+
 public class Node {
 	private int value;
 	private Node next = null;
 	private Node previous = null;
 	
+	/**
+	 * Build a new node with only the node value
+	 * 
+	 * @param value Value that the node will have
+	 * */
 	public Node(int value) {
 		this.value = value;
 	}
+	/**
+	 * Build a node by setting its value and the reference to the previous node.
+	 * 
+	 * @param value Value contained in the node
+	 * @param previous Node previous to the node being created
+	 * */
 	public Node(int value, Node previous) {
 		this.value = value;
 		this.previous = previous;
 	}
+	/**
+	 * Create a node with a value and references to both the next node and the previous node.
+	 * 
+	 * @param value Value contained in the node
+	 * @param previous Node previous to the node being created
+	 * @param next Node next to the node being created
+	 * */
 	public Node(int value, Node previous, Node next) {
 		this.value = value;
 		this.previous = previous;
 		this.next = next;
 	}
 	
+	/**
+	 * Get the value contained by the node
+	 * 
+	 * @return Returns the value of the node
+	 * */
 	public int getValue() {return this.value;}
+	/**
+	 * Modifies the value contained by the node
+	 * 
+	 * @param newValue New value that will contain node 
+	 * */
 	public void setValue(int newValue) {this.value = newValue;}
 	
+	/**
+	 * Get a reference to the next node
+	 * 
+	 * @return Reference to the next node of the current node
+	 * */
 	public Node getNextNode() {return this.next;}
+	/**
+	 * Modify the following node of the current node
+	 * 
+	 * @param newNextNode New reference to a node
+	 * */
 	public void setNextNode(Node newNextNode) {this.next = newNextNode;}
 	
+	/**
+	 * Gets the node referenced as the one before the current node
+	 * 
+	 * @return Reference to a node
+	 * */
 	public Node getPreviousNode() {return this.previous;}
+	/**
+	 * Modify the reference to the previous node
+	 * 
+	 * @param newPreviousNode New reference to a node
+	 * */
 	public void setPreviousNode(Node newPreviousNode) {this.previous = newPreviousNode;}
 	
+	/**
+	 * Create a string with/display the most important information about the node, the value
+	 * 
+	 * @return Returns the most important information about the node as a string
+	 * */
 	public String toString() {
-		return "[value: " + this.value + "\n";
+		return "[value: " + this.value + "]\n";
 	}
+	/**
+	 * Checks whether an object is equal to the current node using the node value as a reference
+	 * 
+	 * @param otherObject Object with which the current node is to be compared
+	 * 
+	 * @return Returns a boolean value. true if both are equal, false if they are not
+	 * */
 	public boolean equals(Object otherObject) {
 		if (this == otherObject) {return true;}
 		if (otherObject == null) {return false;}
